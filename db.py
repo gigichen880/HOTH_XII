36bcdd331f0f913c0b3f0632f6fc89d86c14a23f
Athena Mo <Athena_1224@outlook.com>
2025-03-02 10:51:50 -0800

    Update db.py

--- db.py
+++ db.py
@@ -5,34 +5,19 @@ from bson import ObjectId
 
 
 class User:
-    def __init__(self, username, password, email):
+    def __init__(self, username, password, email, verify):
         self.username = username
         self.password = password
         self.email = email
+        self.verify = False
 
 
-class Listing:
-    def __init__(self, name=None, location=None, time=None, duration=None, price=None, description=None, username=None, user_accepted=None, bids=None, ready=None):
-        if name:
-            self.name = name
-        if location:
-            self.location = location
-        if time:
-            self.time = time
-        if duration:
-            self.duration = duration
-        if price:
-            self.price = price
-        if description:
-            self.description = description
-        if username:
-            self.username = username
-        if user_accepted:
-            self.user_accepted = user_accepted
-        if bids:
-            self.bids = bids
-        if ready:
-            self.ready = ready
+class Room:
+    def __init__(self, courseName=None, onlineUsers=None):
+        if courseName:
+            self.courseName = courseName
+        if onlineUsers:
+            self.onlineUsers = []
 
 
 class Database:
@@ -66,6 +51,6 @@ class Database:
             print(f"Error getting user: {str(e)}")
             return None
 
-    def add_listing(self, listing: Listing):
-        return self.db.listings.insert_one(vars(listing)).inserted_id
+    def add_room(self, room: room):
+        return self.db.room.insert_one(vars(room)).inserted_id
 
