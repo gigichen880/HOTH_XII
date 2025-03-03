4853b1d5dffb3c202dc51270b2a7f482d94ffce3
Samaritan001 <999000111@sina.com>
2025-03-02 20:20:50 -0800

    solved css alignment issue

--- hoth/src/room.js
+++ hoth/src/room.js
@@ -1,47 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./room.css";
 
-import "./room.css"
+function Room() {
+  const location = useLocation();
+  const major_name = location.state?.major_name;
+  let navigate = useNavigate();
 
-/*
-export default function Room()
-{
-    const [url, setUrl] = useState("https://localhost:5501");
-    return (
-        <div>
-            <button onClick={() => setUrl("https://localhost:5501/hoth/src/video_stream/index.html")}>Join Stream</button>
-            <iframe src={url} width="100%" height="600px" style={{ border: "none"}}/>
-        </div>
-    )
+  return (
+    <div style={{ width: "100%", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="http://127.0.0.1:5501/hoth/src/video_stream/index.html"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+      ></iframe>
+    </div>
+  );
 }
-*/
 
-
-
-function Room()
-{
-    const location = useLocation();
-    const major_name = location.state?.major_name;
-    let navigate = useNavigate();
-    return (
-        <div>
-            <h1 id="title">Meeting Room</h1>
-            <button onClick={() => {
-                navigate(`/major/${major_name}`);
-            }}>Exit</button>
-            <image src="./button.png" style={{width: '100%', height: '100vh'}}></image>
-            <div style={{ width: '100%', height: '100vh' }}>
-                
-                <iframe
-                    src="http://127.0.0.1:5501/hoth/src/video_stream/index.html" // or use /static-html/index.html if in public folder
-                    width="100%"
-                    height="100%"
-                    style={{ border: 'none' }}
-                ></iframe>
-            </div>
-        </div>
-    );
-}
-export default Room
-
-
+export default Room;
